1f9ca917e815c7e3656c21e5e03117203614ceea
porChe1223 <houshinri@gmail.com>
2024-09-10 23:02:48 +0900

    no.2-Tweet作成処理・詳細画面・更新処理・削除処理の実装

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -12,7 +12,8 @@ class TweetController extends Controller
      */
     public function index()
     {
-        //
+        $tweets = Tweet::with('user')->latest()->get();
+        return view('tweets.index', compact('tweets'));
     }
 
     /**
@@ -20,8 +21,7 @@ class TweetController extends Controller
      */
     public function create()
     {
-        $tweets = Tweet::with('user')->latest()->get();
-        return view('tweets.index', compact('tweets'));
+        return view('tweets.create');
     }
 
     /**
@@ -29,7 +29,13 @@ class TweetController extends Controller
      */
     public function store(Request $request)
     {
-        return view('tweets.create');
+        $request->validate([
+            'tweet' => 'required|max:255',
+        ]);
+
+        $request->user()->tweets()->create($request->only('tweet'));
+
+        return redirect()->route('tweets.index');
     }
 
     /**
@@ -37,7 +43,7 @@ class TweetController extends Controller
      */
     public function show(Tweet $tweet)
     {
-        //
+        return view('tweets.show', compact('tweet'));
     }
 
     /**
@@ -45,7 +51,7 @@ class TweetController extends Controller
      */
     public function edit(Tweet $tweet)
     {
-        //
+        return view('tweets.edit', compact('tweet'));
     }
 
     /**
@@ -53,7 +59,13 @@ class TweetController extends Controller
      */
     public function update(Request $request, Tweet $tweet)
     {
-        //
+        $request->validate([
+            'tweet' => 'required|max:255',
+        ]);
+
+        $tweet->update($request->only('tweet'));
+
+        return redirect()->route('tweets.show', $tweet);
     }
 
     /**
@@ -61,6 +73,8 @@ class TweetController extends Controller
      */
     public function destroy(Tweet $tweet)
     {
-        //
+        $tweet->delete();
+
+        return redirect()->route('tweets.index');
     }
 }
