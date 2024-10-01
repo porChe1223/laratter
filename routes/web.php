95bd41aa784d835900050578f9d9175b3d5115b7
porChe1223 <houshinri@gmail.com>
2024-10-02 02:57:33 +0900

    20241001-1___カートからOderテーブルに追加20240927-1___フォロー機能

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TweetController;
 use App\Http\Controllers\TweetLikeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FollowController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -19,6 +20,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::get('/profile/{user}', [ProfileController::class, 'show'])->name('profile.show');
+    Route::post('/follow/{user}', [FollowController::class, 'store'])->name('follow.store');
+    Route::delete('/follow/{user}', [FollowController::class, 'destroy'])->name('follow.destroy');
     Route::get('/tweets/search', [TweetController::class, 'search'])->name('tweets.search');
     Route::resource('tweets', TweetController::class);
     Route::post('/tweets/{tweet}/like', [TweetLikeController::class, 'store'])->name('tweets.like');
